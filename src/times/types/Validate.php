0d136eb89f062b1bfae078a84b87fb7dddbf5aa8
gaozhe <851947523@qq.com>
2024-01-26 16:17:33 +0800

    更新是否闰年，是否指定日期之前或者之后

--- src/times/types/Validate.php
+++ src/times/types/Validate.php
@@ -37,44 +37,40 @@ trait Validate
     //对象的年份是否是闰年。
     public function isLeapYear()
     {
-        $value = $this->unix();
-        var_dump($value);exit;
+        $value = $this->unix()->getValue();
+        $time = mktime(20,20,20,2,1,date('Y',$value));//取得一个日期的 Unix 时间戳;
+        if (date("t",$time)== 29){ //格式化时间，并且判断2月是否是29天；
+            return true;
+        }else{
+            return false;
+        }
 
     }
 
     //是否在另一个提供的日期时间之前。
     public function isBefore($time = '')
     {
+        $startTime = $this->unix()->getValue();
+        $endTime = is_string($time) ? strtotime($time) : $time;
+        return $startTime <= $endTime;
 
     }
 
     //供的日期时间相同
     public function isSame()
     {
-
+        $startTime = $this->unix()->getValue();
+        $endTime = is_string($time) ? strtotime($time) : $time;
+        return $startTime == $endTime;
     }
 
     //对象是否在另一个提供的日期时间之后。
     public function isAfter()
     {
+        $startTime = $this->unix()->getValue();
+        $endTime = is_string($time) ? strtotime($time) : $time;
+        return $startTime >= $endTime;
 
     }
 
-    //对象是否和另一个提供的日期时间相同或在其之前。
-    public function isSameOrBefore()
-    {
-
-    }
-
-    //对象是否和另一个提供的日期时间相同或在其之后。
-    public function isSameOrAfter()
-    {
-
-    }
-
-    //对象是否在其他两个的日期时间之间。
-    public function isBetween()
-    {
-
-    }
 }
